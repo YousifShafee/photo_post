898eebbd1e5480c5f801807c8c9ea3e962f13977
YousifShafee <yousiftariq1@hotmail.com>
2023-05-15 16:59:22 +0300

    Registration Page

---
+++ src/main/java/com/photo_service/photo_post/controller/PublicController.java
@@ -0,0 +1,41 @@
+package com.photo_service.photo_post.controller;
+
+import com.photo_service.photo_post.model.Person;
+import com.photo_service.photo_post.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("public")
+public class PublicController {
+
+    @Autowired
+    PersonService personService;
+
+    @RequestMapping(value ="/register",method = { RequestMethod.GET})
+    public String displayRegisterPage(Model model) {
+        model.addAttribute("person", new Person());
+        return "register.html";
+    }
+
+    @RequestMapping(value ="/createUser",method = { RequestMethod.POST})
+    public String createUser(@Valid @ModelAttribute("person") Person person, Errors errors) {
+        if(errors.hasErrors()){
+            return "register.html";
+        }
+        boolean isSaved = personService.createNewPerson(person);
+        if(isSaved){
+            return "redirect:/login?register=true";
+        }else {
+            return "register.html";
+        }
+    }
+
+}
